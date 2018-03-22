c0b3218c3fef31e0c71d704457c8d5546f7a608e
aldianfebriyanto <aldi270201@gmail.com>
2018-03-22 16:19:50 +0700

    Home Finally
    Home

---
+++ home/it/follow-us.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<!-- saved from url=(0036)https://s.bootsnipp.com/iframe/Qb83E -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="robots" content="noindex, nofollow">
+
+    <title></title>
+
+    <!--Bootstrap & CSS-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../assets/css/bootstrap.min(1).css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" type="text/css" href="style_it.css">
+    <script src="../assets/js/jquery-1.11.1.min.js.download"></script>
+    <script src="../assets/js/bootstrap.min.js(1).download"></script>
+     <link rel="shortcut icon" type="image/x-icon" href="../img/ffcico.ico"/>
+    <!--Bootstrap & CSS-->
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/js/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="..assets/css/stylish-portfolio.min.css" rel="stylesheet">
+    <link href="..assets/css/stylish-portfolio.css" rel="stylesheet">
+
+</head>
+<body>
+  
+  <!--HEADER START-->
+	    <div id="wrapper">
+        <div class="overlay"></div>
+    
+        <!-- Sidebar -->
+       <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation" style="background-color: #008080;">
+            <ul class="nav sidebar-nav">
+                <li class="sidebar-brand">
+                    <a class="navbar-brand js-scroll-trigger" href="../index.html"><img src="../img/kaefce.png" style="height: 70px; margin-top: 10px;"></a>
+                </li>
+                <li>
+                    <a href="index.php" style="margin-top: 40px;">Information Technology</a>
+                </li>
+
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">IT Infrastructure<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-header">   </li>
+                    <li><a href="vision_page_infra.php">Visi & Misi</a></li>
+                    <li><a href="biodata.php">Biodata Karyawan</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">IT ERD<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-header">   </li>
+                    <li><a href="vision_page_erd.php">Visi & Misi</a></li>
+                    <li><a href="biodata.php">Biodata Karyawan</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">IT Support<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-header">   </li>
+                    <li><a href="vision_page_support.php">Visi & Misi</a></li>
+                    <li><a href="biodata.php">Biodata Karyawan</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">IT Solution<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-header">   </li>
+                    <li><a href="vision_page_solution.php">Visi & Misi</a></li>
+                    <li><a href="biodata.php">Biodata Karyawan</a></li>
+                  </ul>
+                </li>
+                
+                <li>
+                    <a href="../it/contact.php">Contact Us</a>
+                </li>
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+
+
+                <li>
+                    <a href="follow-us.php">Our Team</a>
+                </li>
+
+            </ul>
+      </nav>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+          <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				  <span class="hamb-bottom"></span>
+            </button>
+
+        <!-- Services -->
+    <section class="content-section bg-primary text-white text-center" id="services" style="background-color: white; height: 626px;">
+      <div class="container">
+        
+        <div class="row" style="background-color: #dddddd; border-radius: 15px; height: 500px;">
+          <h1 style="color: black;">Our Team</h1>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <img src="../img/Steam_Logo.png" style="height: 150px; width: 150px; margin-top: 50px;">
+            <h4>
+              <strong style="color: black;">Aldian Febriyanto</strong>
+            </h4>
+            <p class="text-faded mb-0" style="color: black;">SMK BAKTI IDHATA</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <img src="../img/Steam_Logo.png" style="height: 150px; width: 150px; margin-top: 50px;">
+            <h4>
+              <strong style="color: black;">Christian Sheva Alexander</strong>
+            </h4>
+            <p class="text-faded mb-0" style="color: black;">SMK BAKTI IDHATA</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <img src="../img/Steam_Logo.png" style="height: 150px; width: 150px; margin-top: 50px;">
+            <h4>
+              <strong style="color: black;">Fariz Azhar Khalied</strong>
+            </h4>
+            <p class="text-faded mb-0" style="color: black;">SMK BAKTI IDHATA</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <img src="../img/Steam_Logo.png" style="height: 150px; width: 150px; margin-top: 50px;">
+            <h4>
+              <strong style="color: black;">Vito Raditya Fauzan</strong>
+            </h4>
+            <p class="text-faded mb-0" style="color: black;">SMK BAKTI IDHATA</p>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+            <div class="footer-bottom" style="background-color: #008080; margin-top: 50px; padding-top: 15px; padding-bottom: 5px;">
+     <div class="container">
+     
+
+          <h5 style="text-align: center; margin-top: 5px;"> &copy Copyright 2018 by teamERROR404.</h5>
+          <div class="credits">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+        </div>
+  </div> 
+  </div>     
+        <!-- /#page-content-wrapper -->
+
+    
+
+        <!--script-->
+
+	<script type="text/javascript">
+
+	$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
+	</script>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="../assets/js/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="../assets/js/stylish-portfolio.min.js"></script>
+
+        <!--script end-->
+
+
+</body>
+</html>
